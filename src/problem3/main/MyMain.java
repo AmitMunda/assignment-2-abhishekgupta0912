dd9808bf4576079e788d5cad7ce3df319f641986
abhishek <abhishek.gupta1_cs18@gla.ac.in>
2020-03-27 02:36:04 +0530

    problem 2 and 3 done

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -5,7 +5,24 @@
  *  Time: 7:13 PM
  */
 package problem3.main;
+
+import problem3.myqueue.MyPriorityQueue;
+
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+    public static void main(String[] args) {
+        MyPriorityQueue pq = new MyPriorityQueue();
+        pq.push("ABHISHEK GUPTA", 1);
+        pq.push("ABHI GUPTA", 5);
+        pq.push("A GUPTA", 2);
+        pq.push("EK GUPTA", 9);
+        pq.push("AB GUPTA", 7);
+        pq.push("SHEK GUPTA", 10);
+        pq.push("H GUPTA", 0);
+
+        pq.display();
+
+
+    }
 }
